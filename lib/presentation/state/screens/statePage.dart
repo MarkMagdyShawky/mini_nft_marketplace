99c29016ad92a926d4601f543861851d43838209
Mark <markmarkmagdy1010@gmail.com>
2024-05-07 06:09:25 +0300

    initialize pages

---
+++ lib/presentation/state/screens/statePage.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class StatePage extends StatelessWidget {
+  const StatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
