6ab62ee916e7813e8e0b2a1d209f7cca597a79f6
Mark <markmarkmagdy1010@gmail.com>
2024-05-17 01:04:40 +0300

    clean the code

--- lib/presentation/onboarding/screens/onboardingPage.dart
+++ lib/presentation/onboarding/screens/onboardingPage.dart
@@ -1,11 +1,41 @@
+import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:mini_nft_marketplace/core/resources/TextManager.dart';
+import 'package:mini_nft_marketplace/core/resources/colorManager.dart';
+import 'package:mini_nft_marketplace/core/resources/imageManager.dart';
+import 'package:mini_nft_marketplace/core/resources/stringManagement.dart';
+import 'package:mini_nft_marketplace/presentation/onboarding/widgets/customOnboardingCard.dart';
+import 'package:mini_nft_marketplace/presentation/onboarding/widgets/customOnboardingImageBg.dart';
+import 'package:mini_nft_marketplace/presentation/onboarding/widgets/customOnboardingTitle.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            // Image Background
+            CustomOnboardingImageBg(),
+            Column(
+              children: [
+                // Main Title
+                CustomOnboardingTitle(),
+                SizedBox(
+                  height: 300,
+                ),
+                // Box
+                CustomOnboardingCard(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
