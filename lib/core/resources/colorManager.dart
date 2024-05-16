c38e82ef58de1af680b7626fe1619e0a97314926
Mark <markmarkmagdy1010@gmail.com>
2024-05-17 01:08:32 +0300

    add new colors

---
+++ lib/core/resources/colorManager.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff5C609B);
+const kSecondaryColor = Color(0xff767CB2);
+const kPThirdColor = Color(0xfff8dff2);
+const kFontColor1 = Colors.white;
+
+const kPrimaryColor2 = Color(0xff211134);
