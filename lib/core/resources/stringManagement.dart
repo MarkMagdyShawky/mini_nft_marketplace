796b656110815f47ac215f795cae23bfbf47e67b
Mark <markmarkmagdy1010@gmail.com>
2024-05-17 01:03:27 +0300

    String Manager that include most common tests 'strings' in the app.......

---
+++ lib/core/resources/stringManagement.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StringManager {
+  static const String onboardingTitle = "Welcome to \nNFT Marketplace" ;
+  static const String onboardingBoxTitle = "Explore and Mint NFTs" ;
+  static const String onboardingBoxSubtitle = "You can buy and sell the NFTs of the best artists in the world." ;
+  static const String onboardingBtn = "Get Start now";
+//   ///////////////////////////////////////////////////////////////
+  static const String HomeTitle = "NFT MarketPlace";
+}
