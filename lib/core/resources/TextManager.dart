9b20cebb1e23291674237f37ebc2078bd2cce5af
Mark <markmarkmagdy1010@gmail.com>
2024-05-17 01:01:43 +0300

    Text Manager that include text widget to format text .......

---
+++ lib/core/resources/TextManager.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:mini_nft_marketplace/core/resources/colorManager.dart';
+
+class TextManagerBold extends StatelessWidget {
+  final String myText;
+  final double FontSize;
+
+  const TextManagerBold(
+      {super.key, required this.myText, required this.FontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "$myText",
+      style: TextStyle(
+          fontSize: FontSize, fontWeight: FontWeight.bold, color: kFontColor1),
+      textAlign: TextAlign.left,
+    );
+  }
+}
+
+class TextManagerNormal extends StatelessWidget {
+  final String myText;
+  final double FontSize;
+
+  const TextManagerNormal(
+      {super.key, required this.myText, required this.FontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "$myText",
+        style: TextStyle(fontSize: FontSize, color: kFontColor1),
+      ),
+    );
+  }
+}
+
+class TextManagerThin extends StatelessWidget {
+  final String myText;
+  final double FontSize;
+
+  const TextManagerThin(
+      {super.key, required this.myText, required this.FontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "$myText",
+        style: TextStyle(
+            fontSize: FontSize,
+            color: kFontColor1,
+            fontFamily: "Montserrat-ExtraLight" , fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
