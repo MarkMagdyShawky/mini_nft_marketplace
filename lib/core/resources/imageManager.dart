fe307eb2a0c5991ec696f43b50837eaf39bab522
Mark <markmarkmagdy1010@gmail.com>
2024-05-17 05:37:24 +0300

    add images

--- lib/core/resources/imageManager.dart
+++ lib/core/resources/imageManager.dart
@@ -1,14 +1,51 @@
-
 import 'package:mini_nft_marketplace/presentation/home/screens/homePage.dart';
 
 class ImageManager {
   static const OnboardingImage = "assets/images/onboarding2.png";
 
+/////////////////////////////////////////
 
   static const HomePageCategory1 = "assets/images/category1.jpg";
   static const HomePageCategory2 = "assets/images/category2.jpg";
   static const HomePageCategory3 = "assets/images/category3.jpg";
 
-  static const List<String> categoryImages= [ImageManager.HomePageCategory2 , ImageManager.HomePageCategory1,ImageManager.HomePageCategory3];
+  static const List<String> categoryImages = [
+    ImageManager.HomePageCategory2,
+    ImageManager.HomePageCategory1,
+    ImageManager.HomePageCategory3
+  ];
 
+  static const HomePageCollection1 = "assets/images/cardCollection1.jpg";
+  static const HomePageCollection2 = "assets/images/cardCollection2.jpg";
+  static const HomePageCollection3 = "assets/images/cardCollection3.jpg";
+  static const HomePageCollection4 = "assets/images/cardCollection4.jpg";
+
+  static const List<String> collectionsImages = [
+    ImageManager.HomePageCollection1,
+    ImageManager.HomePageCollection2,
+    ImageManager.HomePageCollection3,
+    ImageManager.HomePageCollection4
+  ];
+
+  static const HomePageTopSeller1 = "assets/images/topSeller1.jpg";
+  static const HomePageTopSeller2 = "assets/images/topSeller2.jpg";
+  static const HomePageTopSeller3 = "assets/images/topSeller3.jpg";
+  static const HomePageTopSeller4 = "assets/images/topSeller4.jpg";
+  static const HomePageTopSeller5 = "assets/images/topSeller5.jpg";
+  static const HomePageTopSeller6 = "assets/images/topSeller6.jpg";
+  static const HomePageTopSeller7 = "assets/images/topSeller7.jpg";
+  static const HomePageTopSeller8 = "assets/images/topSeller8.jpg";
+
+  static const List<String> topSellerImages = [
+    ImageManager.HomePageTopSeller1,
+    ImageManager.HomePageTopSeller2,
+    ImageManager.HomePageTopSeller3,
+    ImageManager.HomePageTopSeller4,
+    HomePageTopSeller5,
+    HomePageTopSeller6,
+    HomePageTopSeller7,
+    HomePageTopSeller8
+  ];
+
+  static const String ethereumHomePage = "assets/images/ethereum.png";
 }
