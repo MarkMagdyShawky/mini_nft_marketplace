aba51186ed5a3f793119f096e4c41995a602159a
Mark <markmarkmagdy1010@gmail.com>
2024-05-17 05:38:03 +0300

    add string for collections and top seller

--- lib/core/resources/stringManagement.dart
+++ lib/core/resources/stringManagement.dart
@@ -9,4 +9,17 @@ class StringManager {
   static const String HomeTitle = "NFT MarketPlace";
   static const List<String> CategoriesCaption =  ["Music","Art","Virtual Worlds"];
 
+  static const String HomepageCollectionTitle = "Trending Collections";
+  static const List<String> CollectionsCaption =  ["3D Art","Abstract Art","Portrait Art" , "Metaverse"];
+
+  static const List<String> CollectionLikesNumbers =  ["200","250","300","600","120"];
+
+  static const String HomepageTopSellerTitle = "Top Seller";
+  static const List<String> TopSellerTitles=  ["Wave","Abstract Pink","Wave","Abstract23","Music","Ball","Ring","Beer"];
+  static const List<String> TopSellerSubtitles=  ["wave2 #5672","abstract #2653","wavepi25","abstract23 #6926","mail #3456","balllii #556","Ring","Beer #627"];
+  static const List<String> TopSellerEthereum =  ["0.815","0.26","0.023","0.641","0.034","0.125","0.058","0.012"];
+
+  static const List<String> TopSellerLikesNumbers =  ["8500","2050","3050","600","120","400","100","80"];
+
+
 }
