7538f13880d5cf4ddc6de19e023e06388a591149
Mark <markmarkmagdy1010@gmail.com>
2024-06-07 05:22:34 +0300

    add tap bar with 2 taps ... Finish ...

--- lib/core/resources/colorManager.dart
+++ lib/core/resources/colorManager.dart
@@ -5,6 +5,7 @@ const kPrimaryColor = Color(0xff5C609B);
 const kSecondaryColor = Color(0xff767CB2);
 const kPThirdColor = Color(0xfff8dff2);
 const kFontColor1 = Colors.white;
+const kBrightColor = Color(0xff9f77ff);
 
 const kBackgroundColor2 = Color(0xff211134);
 
--- lib/presentation/state/screens/statePage.dart
+++ lib/presentation/state/screens/statePage.dart
@@ -22,54 +22,102 @@ class StatePage extends StatelessWidget {
     double heightScreen = MediaQuery.of(context).size.height;
     int cardsLength = ImageManager.stateCardImages.length;
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor2,
-      drawer: MarkDrawer(),
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         backgroundColor: kBackgroundColor2,
-        toolbarHeight: 60,
-        title: TextManagerNormal(
-            myText: StringManager.StatePagTitle, FontSize: 21),
-        iconTheme: IconThemeData(color: kFontColor1),
-        centerTitle: true,
-        actions: [
-          PopupMenuButton(
-              color: kPrimaryColor.withOpacity(0.5),
-              offset: Offset(6, 6),
-              itemBuilder: (context) => [
-                    PopupMenuItem(
-                        child: TextManagerThin(
-                            myText: StringManager.StatePageRanking,
-                            FontSize: 12),
-                        value: "Ranking"),
-                    PopupMenuItem(
-                        child: TextManagerThin(
-                            myText: StringManager.StatePageActivity,
-                            FontSize: 12),
-                        value: "Activity")
-                  ])
-        ],
-      ),
-      body: SafeArea(
-          child: Stack(
-        children: [
-          // for background
-          StateImageBackground(),
-          StateCardBackground(),
-          ListView(
-            children: [
-              CustomDropMenu(),
-              // Card
-              Container(
-                height: heightScreen,
-                width: double.infinity,
-                child: CustomCardStatePage(),
-              )
+        drawer: MarkDrawer(),
+        appBar: AppBar(
+          backgroundColor: kBackgroundColor2,
+          toolbarHeight: 80,
+          title: TextManagerNormal(
+              myText: StringManager.StatePagTitle, FontSize: 21),
+          iconTheme: IconThemeData(color: kFontColor1),
+          centerTitle: true,
+          actions: [
+            PopupMenuButton(
+                color: kPrimaryColor.withOpacity(0.5),
+                offset: Offset(6, 6),
+                itemBuilder: (context) => [
+                      PopupMenuItem(
+                          child: TextManagerThin(
+                              myText: StringManager.StatePageRanking,
+                              FontSize: 12),
+                          value: "Ranking"),
+                      PopupMenuItem(
+                          child: TextManagerThin(
+                              myText: StringManager.StatePageActivity,
+                              FontSize: 12),
+                          value: "Activity")
+                    ])
+          ],
+          bottom: TabBar(
+
+            tabs: [
+              Tab(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.bar_chart_sharp,
+                      color: kFontColor1,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    TextManagerThin(myText: "Ranking", FontSize: 14),
+                  ],
+                ),
+              ),
+              Tab(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.area_chart_sharp,
+                      color: kFontColor1,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    TextManagerThin(myText: "Activity", FontSize: 12),
+                  ],
+                ),
+              ),
             ],
+            indicatorColor: kBrightColor,
+            dividerColor: kPrimaryColor.withOpacity(0.5),
+            indicatorWeight: 5,
           ),
-          // 2 lists
-        ],
-      )),
+        ),
+        body: SafeArea(
+            child: Stack(
+          children: [
+            // for background
+            StateImageBackground(),
+            StateCardBackground(),
+            ListView(
+              children: [
+                SizedBox(height: 30,),
+                // 2 lists
+                CustomDropMenu(),
+                SizedBox(height: 15,),
+                // Card
+                Container(
+                  height: heightScreen,
+                  width: double.infinity,
+                  child: CustomCardStatePage(),
+                )
+              ],
+            ),
+            // 2 lists
+          ],
+        )),
+      ),
     );
   }
 }
